3c57b8bc5a2177c1eaf1897bef25ee6959b81963
fepe15 <fedegperalta@gmail.com>
2019-02-26 20:27:19 -0300

    Descargar ticket en pdf

--- TP_LAB4-master/src/app/mesas/mesas.component.ts
+++ TP_LAB4-master/src/app/mesas/mesas.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { MesasService } from '../services/mesas.service';
 import { AuthService } from '../services/auth/auth.service';
+import { min } from 'rxjs/operators';
+
+import * as jspdf from 'jspdf';  
+  
+import html2canvas from 'html2canvas';
 
 export interface DetalleMesas {
   nroMesa: string;
@@ -19,6 +24,9 @@ export class MesasComponent implements OnInit {
   importe;
   display: boolean = false;
   perfil;
+  mesaFactura:number;
+  fechaActual:string;
+  horaActual:string;
 
   displayedColumns: string[] = ['nroMesa', 'estado', 'acciones'];
 
@@ -29,8 +37,6 @@ export class MesasComponent implements OnInit {
   
     this.TraerLasMesas();
    }
-
- 
 
   TraerLasMesas()
   {
@@ -52,11 +58,15 @@ export class MesasComponent implements OnInit {
 
   Cobrar(idMesa)
   {
+    this.TraerFechaActual();
+    this.mesaFactura=idMesa;
     this.httpMesa.CobrarMesa(idMesa)
     .subscribe((data)=>{
      // console.log(data);
       this.importe= JSON.parse(data._body).total;
+      if(this.importe != null){
       this.display=true;
+    }
       this.TraerLasMesas();
     });
   }
@@ -71,8 +81,37 @@ export class MesasComponent implements OnInit {
     });
   }
 
+  TraerFechaActual(){
+    var hoy = new Date();
+    var hours:string = hoy.getHours().toString();
+    var mins:string = hoy.getMinutes().toString();
+    var day:string = hoy.getDate().toString();
+    var month:string = hoy.getMonth().toString();
+    var year:string = hoy.getFullYear().toString();
+    this.fechaActual =  day.padStart(2,'0') + '/' + month.padStart(2,'0') + '/' + year;
+    this.horaActual = hours.padStart(2,'0')+':'+ mins.padStart(2,'0');
+  }
 
   ngOnInit() {
   }
+
+
+  public captureScreen()  
+  {  
+    var data = document.getElementById('contentToConvert');  
+    html2canvas(data).then(canvas => {  
+      // Few necessary setting options  
+      var imgWidth = 52;   
+      var pageHeight = 74;    
+      var imgHeight = 74;   
+      var heightLeft = imgHeight;  
+  
+      const contentDataURL = canvas.toDataURL('image/png')  
+      let pdf = new jspdf('p', 'mm', 'a8'); // A4 size page of PDF  
+      var position = 0;  
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)  
+      pdf.save('ticket.pdf'); // Generated PDF   
+    });  
+  }  
 
 }
